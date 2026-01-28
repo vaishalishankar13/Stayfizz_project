9ca553ff6e91e370a80944076523633230bc10f3
Vaishali Shankar <vaishalishankar13@gmail.com>
2026-01-29 01:30:28 +0530

    Upgrade multer-storage-cloudinary to v4 and fix Cloudinary config

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,20 +1,21 @@
-const cloudinary = require('cloudinary').v2;
-const multerStorage = require('multer-storage-cloudinary');
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
-    cloud_name:process.env.CLOUD_NAME,
-    api_key:process.env.CLOUD_API_KEY,
-    api_secret:process.env.CLOUD_API_SECRET
-})
-
-const storage = new multerStorage.CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'stayfrizz_DEV',
-    allowedFormats: ["jpg" ,"png" ,"jpeg"], // supports promises as well
-  },
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
 });
- 
-module.exports={
-    cloudinary,storage
-}
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "Stayfizz",
+    allowed_formats: ["jpeg", "png", "jpg"]
+  }
+});
+
+module.exports = {
+  cloudinary,
+  storage
+};
